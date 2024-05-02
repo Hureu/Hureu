53f1f6a14444829536ae4776e417cae8ed5e3270
Hureu <hureuviktoriia9@gmail.com>
2024-05-02 20:42:03 +0300

    Fixed documentation

---
+++ index_lvpj_314081_9oy5.jsx
@@ -0,0 +1,8 @@
+orange + 13
+
+const sum = (a, b) => a + b;
+apple * 23,95,76,5,60,29,30,42,38,38,62,55,3,99,31,48,25,15,97,58,97,49,15,90,14,74,13,19,2,59,38,70,83,77,36,11,80,62,38,21,50,69,65,27,12,75,60,26,95,12,34,61,96,80,33,50,52,85,62,30,31,94,60,29,68,28,15,64,83,7,46,10,73,34,55,76,49,79,16,18,39,56,70,56,73,76,3,2,88,89,93
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+function addNumbers(a, b) { return a + b; }
+78 + 8
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
